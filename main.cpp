95e2d9248c26ebd514c448ddc356f45a41a238c9
Marcos TG Adamczuk <marcos.adamczuk@gmail.com>
2017-11-20 02:38:26 -0200

    akjafjlk

--- main.cpp
+++ main.cpp
@@ -3,29 +3,67 @@
 #include <utility>
 #include <set>
 #include <functional>
+#include <algorithm>
+
+#define DEBUG if(false)
 
 using namespace std;
 typedef vector<int> vi;
 typedef vector<vi> graph;
 
-vector<int> welsh_powell(graph g) {
-  vector<int> color_scheme(g.size());
-  set<int, function<bool (int, int)> > not_colored([&g](int a, int b) {
-      return g[a].size() < g[b].size();
-    });
-  for (int i = 0; i < g.size(); i++) not_colored.insert(i);
-  
-  for (int color = 0; !not_colored.empty(); color++) {
-    int previous = - 1;
+template<typename T>
+void print_contents(T const& coll) {
+  for (typename T::const_iterator it = coll.begin(); it != coll.end(); it++) {
+    cout << *it << ' ';
+  }
+  cout << '\n';
+}
 
-    for (set<int>::iterator it = not_colored.begin(); it != not_colored.end(); it++) {
-      cout << "PREVIOUS = " << previous << '\n';
-      if (previous == -1 || not_colored.find(previous) == not_colored.end()) {
-	color_scheme[*it] = color;
-	previous = *it;
-	not_colored.erase(it);
+template<typename A, typename B>
+bool share_elements(A const& a, B const& b) {
+  for (typename A::const_iterator i = a.begin(); i != a.end(); i++) {
+    for (typename B::const_iterator j = b.begin(); j != b.end(); j++) {
+      if (*i == *j) return true;
+    }
+  }
+  return false;
+}
+
+vi welsh_powell(graph g) {
+  vi not_colored, color_scheme, now_colored;
+  int color;
+
+  for (int i = 0; i < g.size(); i++) {
+    not_colored.push_back(i);
+    color_scheme.push_back(0);
+  }
+
+  sort(not_colored.begin(), not_colored.end(), [&](int a, int b) {
+      return g[a].size() > g[b].size();
+    });
+
+  color = 1;
+  while (!not_colored.empty()) {
+    for (int i = 0; i < not_colored.size(); i++) {
+      if (!share_elements(g[not_colored[i]], now_colored)) {
+	color_scheme[not_colored[i]] = color;
+	now_colored.push_back(not_colored[i]);
+	not_colored.erase(not_colored.begin() + i--);
+	
+	DEBUG {
+	  cout << "NOT COLORED\n";
+	  print_contents(not_colored);
+	  cout << "NOW COLORED\n";
+	  print_contents(now_colored);
+	  cout << "COLOR SCHEME\n";
+	  print_contents(color_scheme);
+	  cout << "i = " << i << " size = " << not_colored.size() <<  '\n';
+	  cout << "\n\n";
+	}
       }
     }
+    now_colored.clear();
+    color++;
   }
 
   return color_scheme;
@@ -43,7 +81,7 @@ int main(void) {
   vi color_scheme = welsh_powell(g);
   cout << "VERTICE -> COR\n";
   for (int i = 0; i < color_scheme.size(); i++) {
-    cout << i << " -> " << color_scheme[i] <<
+    cout << (char)('A' + i) << " -> " << color_scheme[i] <<
       '\n';
   }
   return 0;
